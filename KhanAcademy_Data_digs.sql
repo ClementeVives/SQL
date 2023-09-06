d95f59a9f5f5a2bb250cc8f5a2cd3487d213868f
ClementeVives <143741741+ClementeVives@users.noreply.github.com>
2023-09-06 12:53:18 -0400

    Update KhanAcademy_Data_digs.sql

--- KhanAcademy_Data_digs.sql
+++ KhanAcademy_Data_digs.sql
@@ -1,3 +1,16 @@
+/*We’ve curated a set of interesting data sets for you: Top movies, Top countries by population, 
+Solar system objects by size, Marvel characters, Furniture store sales, Earned KA badges, 
+Winston's donut logs, Card game results, and NFL draft picks. 
+
+Pick one of those data sets or create a data set like that, and use advanced SELECT queries to 
+discover things about the data. What sort of questions might one have about that data, like if 
+they were using it for an app or a business idea? Here are some ideas:
+
+What are average, max, and min values in the data?
+What about those numbers per category in the data (using HAVING)?
+What ways are there to group the data values that don’t exist yet (using CASE)?
+What interesting ways are there to filter the data (using AND/OR)? */
+
 /*Earned Badges
 This table contains badges earned by a user, including the most recent date achieved, the type, the name, the # of energy points earned, and the activity earned from.
 Collected by: https://www.khanacademy.org/profile/chopsor/
