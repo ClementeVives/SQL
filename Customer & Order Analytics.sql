a49fcbb5ede6ffbec42cbb249dba443721631936
ClementeVives <143741741+ClementeVives@users.noreply.github.com>
2023-09-12 23:23:24 -0400

    Update Customer & Order Analytics.sql

--- Customer & Order Analytics.sql
+++ Customer & Order Analytics.sql
@@ -4,14 +4,14 @@
 
 SELECT COUNT(orderid) FROM BIT_DB.JanSales
 WHERE length(orderid) = 6 
-AND orderid <> 'Order ID'
+AND orderid <> 'Order ID';
 
 /*RETIRN How many of those orders were for an iPhone*/
   
 SELECT COUNT(orderid) FROM BIT_DB.JanSales
 WHERE Product='iPhone'
 AND length(orderid) = 6 
-AND orderid <> 'Order ID'
+AND orderid <> 'Order ID';
 
 /*RETURN customer account numbers for all the orders that were placed in February*/
   
@@ -19,23 +19,23 @@ SELECT distinct acctnum FROM BIT_DB.customers cust
 INNER JOIN BIT_DB.FebSales Feb
 ON cust.order_id=FEB.orderid
 WHERE length(orderid) = 6 
-AND orderid <> 'Order ID'
+AND orderid <> 'Order ID';
 
 /*RETURN which product was the cheapest one sold in January, and what was the price*/
   
 SELECT distinct product, price FROM BIT_DB.JanSales 
-ORDER BY price ASC LIMIT 1
+ORDER BY price ASC LIMIT 1;
 
 /*RETURN total revenue for each product sold in January*/
   
 SELECT sum(quantity)*price as revenue ,product FROM BIT_DB.JanSales
-GROUP BY product
+GROUP BY product;
 
 /*RETURN which products were sold in February at 548 Lincoln St, Seattle, WA 98101, how many of each were sold, and what was the total revenue*/
   
 SELECT sum(Quantity), product, sum(quantity)*price as revenue FROM BIT_DB.FebSales 
 WHERE location = '548 Lincoln St, Seattle, WA 98101'
-GROUP BY product
+GROUP BY product;
 
 /*RETURN customers that ordered more than 2 products at a time, and what was the average amount spent for those customers*/
   
@@ -44,7 +44,7 @@ LEFT JOIN BIT_DB.customers cust
 ON FEB.orderid = cust.order_id
 WHERE Feb.Quantity>2
 AND length(orderid) = 6 
-AND orderid <> 'Order ID'
+AND orderid <> 'Order ID';
 
 /*RETURN products sold in Los Angeles in February, and include how many of each were sold*/
 
@@ -91,6 +91,6 @@ AND orderid <> 'Order ID';
 /*RETURN which product brought in the most revenue in January and how much revenue did it bring in total*/
 
 SELECT Product, SUM(quantity), sum(quantity*price) FROM BIT_DB.JanSales
-GROUP BY Product
+GROUP BY Product;
 ORDER BY product desc
 LIMIT 1;
